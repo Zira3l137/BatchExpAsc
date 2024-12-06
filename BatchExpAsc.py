d4900e5005847788efdf527dcc96a53af4abd9ab
Zira3l137 <raulbairamov2002@gmail.com>
2024-12-06 13:49:22 +0400

    minor changes

---
+++ BatchExpAsc.py
@@ -0,0 +1,193 @@
+from enum import IntEnum
+from pathlib import Path
+from sys import argv
+
+import bpy
+from krximpexp.BatAscImp import DEFAULT_SAMPLE_MESH_DIR, ASCParser, AscType, BatAscImp
+from krximpexp.KrxAscExp import KrxAscExp
+from krximpexp.scene import SceneMode
+
+DEFAULT_EXPORT_DIR = Path().home().resolve() / "BatchAscExp"
+EXPORT_FORMAT = {0: "fbx", 1: "asc"}
+
+
+def get_armature():
+    for obj in bpy.data.objects:
+        if obj.type == "ARMATURE":
+            return obj
+
+
+class ExportType(IntEnum):
+    FBX = 0
+    ASC = 1
+
+
+class NotAnAnimation(Exception):
+    def __init__(self, message="") -> None:
+        super().__init__(message)
+
+
+class BatchExpAsc:
+    def __init__(
+        self,
+        body_path: str,
+        anim_path: str,
+        # Following parameters are optional
+        export_dir: str | None = None,
+        export_type: int = ExportType.ASC,
+        import_body: bool = True,
+        file_name: str | None = None,
+        export_from_frame: int | None = None,
+        export_to_frame: int | None = None,
+        for_ue: bool = False,
+    ):
+        self.body_path = Path(body_path)
+        self.anim_path = Path(anim_path)
+        self.export_dir = (
+            Path(export_dir) if export_dir else DEFAULT_EXPORT_DIR / self.anim_path.stem
+        )
+        self.export_type = export_type
+        self.import_body: bool = import_body
+        self.file_name: str | None = file_name
+        self.start: int | None = export_from_frame
+        self.end: int | None = export_to_frame
+        self.for_ue: bool = for_ue
+
+        if self.import_body:
+            self._import_body()
+        if not self.anim_path.is_dir():
+            self._export_anim(self.anim_path)
+        else:
+            self.anim_count = 0
+            for anim in self.anim_path.iterdir():
+                if anim.is_file() and anim.suffix.lower() == ".asc":
+                    self.anim_count += 1
+                    self._export_anim(anim)
+
+    def _import_body(self):
+        BatAscImp(
+            str(self.body_path),
+            scene_mode=SceneMode.REPLACE,
+            model_prefix="",
+            sample_meshes_directory=DEFAULT_SAMPLE_MESH_DIR,
+        )
+
+    def _import_anim(self, anim_path: str):
+        self.asc_parser = ASCParser(filename=anim_path)
+        if self.asc_parser.model_type != AscType.DYNAMIC_ANIM:
+            raise NotAnAnimation
+
+        start_frame = self.asc_parser.time_transform.min_frame_in_file
+        end_frame = self.asc_parser.time_transform.max_frame_in_file
+        BatAscImp(
+            anim_path,
+            parser_handle=self.asc_parser,
+            scene_mode=SceneMode.REPLACE_ANIM,
+            model_prefix="",
+            start_frame_in_file=start_frame,
+            end_frame_in_file=end_frame,
+            start_frame_in_scene=start_frame,
+            end_frame_in_scene=end_frame,
+        )
+
+    def _set_frame_range(self):
+        context = bpy.context
+        if not context:
+            raise RuntimeError("No context")
+        obj = context.active_object
+        if not obj:
+            raise RuntimeError("No object selected")
+        if not self.start and not self.end:
+            return
+
+        obj_anim_data = obj.animation_data
+        obj_action = obj_anim_data.action
+        if obj_anim_data and obj_action:
+            first_frame = self.start if self.start else 0
+            last_frame = (
+                self.end
+                if self.end
+                else self.asc_parser.time_transform.end_frame_in_file
+            )
+            context.scene.frame_start = first_frame
+            context.scene.frame_end = last_frame
+
+    def _get_final_path(self, anim_path: Path) -> str:
+        file_stem = anim_path.stem if not self.file_name else self.file_name
+        suffix = EXPORT_FORMAT[self.export_type]
+        self.export_dir.mkdir(parents=True, exist_ok=True)
+        return str((self.export_dir / f"{file_stem}.{suffix}").resolve())
+
+    def _export_anim(self, anim_path: Path):
+        try:
+            self._import_anim(str(anim_path))
+        except NotAnAnimation:
+            print(f"{str(anim_path)} is not an animation, skipping...")
+            return
+        self._set_frame_range()
+        export_file_name = self._get_final_path(anim_path)
+        if self.export_type == ExportType.FBX:
+            self._export_to_fbx(export_file_name)
+            return
+        self._export_to_asc(export_file_name)
+
+    def _export_to_fbx(self, export_file_name: str):
+        bpy.ops.object.select_all(action="SELECT")
+        if self.for_ue:
+            bpy.ops.export_scene.fbx(
+                filepath=export_file_name,
+                bake_anim=True,
+                mesh_smooth_type="EDGE",
+                axis_forward="X",
+                axis_up="Z",
+                object_types={"ARMATURE", "MESH"},
+                use_selection=True,
+                add_leaf_bones=False,
+            )
+        else:
+            bpy.ops.export_scene.fbx(
+                filepath=export_file_name,
+                bake_anim=True,
+                object_types={"ARMATURE", "MESH"},
+                use_selection=True,
+            )
+
+    def _export_to_asc(self, export_file_name: str):
+        armature = get_armature()
+        if not armature:
+            raise RuntimeError("No armature found")
+        bones = armature.data.bones
+        selected_nodes = [bone.name for bone in bones]
+        for obj in bpy.data.objects:
+            if obj.parent_bone in selected_nodes:
+                selected_nodes.append(obj.name)
+        KrxAscExp(
+            export_file_name,
+            export_anim=True,
+            selected_objects_param=selected_nodes,
+            start_frame_in_file=self.asc_parser.time_transform.start_frame_in_file,
+            end_frame_in_file=self.asc_parser.time_transform.end_frame_in_file,
+            start_frame_in_scene=self.asc_parser.time_transform.start_frame_in_scene,
+            end_frame_in_scene=self.asc_parser.time_transform.end_frame_in_scene,
+        )
+
+
+def parse_commandline_arguments() -> dict[str, str]:
+    return {
+        "body_path": argv[5],
+        "anim_path": argv[6],
+        "export_dir": argv[7],
+        "export_type": argv[8],
+        "import_body": argv[9],
+    }
+
+
+if __name__ == "__main__":
+    arguments = parse_commandline_arguments()
+    BatchExpAsc(
+        body_path=arguments["body_path"],
+        anim_path=arguments["anim_path"],
+        export_dir=arguments["export_dir"],
+        export_type=int(arguments["export_type"]),
+        import_body=bool(arguments["import_body"]),
+    )
---
+++ main.py
@@ -0,0 +1,137 @@
+from argparse import ArgumentParser, Namespace
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from configparser import ConfigParser
+from os import cpu_count
+from pathlib import Path
+from subprocess import run
+from time import time
+
+
+def parse_commandline_arguments() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--config_path",
+        "-c",
+        help="Path to the INI config file",
+        type=str,
+        default=Path(__file__).parent / "config.ini",
+    )
+    parser.add_argument(
+        "--max_processes",
+        "-m",
+        help="Maximum number of processes to spawn",
+        type=int,
+        default=0,
+    )
+    return parser.parse_args()
+
+
+def get_path(path: str) -> str:
+    if '"' in path:
+        return path.replace('"', "")
+    return path
+
+
+def spawn_instance(args: tuple) -> int:
+    (
+        blender_path,
+        scene_path,
+        script_path,
+        body_path,
+        anim_path,
+        export_dir,
+        export_type,
+    ) = args
+    import_body = "True" if body_path else ""
+    return run(
+        [
+            blender_path,
+            "--background",
+            scene_path,
+            "--python",
+            script_path,
+            body_path,
+            anim_path,
+            export_dir,
+            export_type,
+            import_body,
+        ],
+    ).returncode
+
+
+def batch_process_anims(
+    blender_path: str,
+    scene_path: str,
+    script_path: str,
+    body_path: str,
+    anims_dir: str,
+    export_dir: str,
+    export_type: str,
+    max_workers: int,
+):
+    anims_query = [
+        (
+            blender_path,
+            scene_path,
+            str(script_path),
+            body_path,
+            str(anim),
+            export_dir,
+            export_type,
+        )
+        for anim in Path(anims_dir).iterdir()
+        if anim.suffix.lower() == ".asc"
+    ]
+
+    executor = ProcessPoolExecutor(max_workers=max_workers)
+    start = time()
+    try:
+        results = executor.map(spawn_instance, anims_query, chunksize=1, timeout=20)
+        executor.shutdown(wait=True)
+    except (OSError, KeyboardInterrupt, AttributeError):
+        executor.shutdown(wait=False, cancel_futures=True)
+        raise
+    end = time()
+    print(f"\n\nTotal time: {end - start:.2f}")
+    return list(results)
+
+    # with ProcessPoolExecutor(max_workers=max_workers) as executor:
+    #     futures = [executor.submit(spawn_instance, args) for args in anims_query]
+    #     start = time()
+    #     for future in as_completed(futures):
+    #         if exception := future.exception():
+    #             print(exception)
+    #     end = time()
+    #     print(f"\n\nTotal time: {end - start:.2f}")
+
+
+def main():
+    arguments = parse_commandline_arguments()
+    config = ConfigParser()
+    config.read(arguments.config_path)
+    max_processes = arguments.max_processes
+    if max_processes == 0:
+        max_processes = max(1, (cpu_count() or 1) * 2)
+
+    SCRIPT_PATH = str(Path(__file__).parent / "BatchExpAsc.py")
+    BLENDER_PATH = get_path(config["Paths"]["blender_exe_path"])
+    SCENE_PATH = get_path(config["Paths"]["blender_scene_path"])
+    BODY_PATH = get_path(config["Paths"]["body_asc_path"])
+    ANIMS_DIR = get_path(config["Paths"]["anims_dir"])
+    EXPORT_DIR = get_path(config["Paths"]["output_dir"])
+    EXPORT_TYPE = get_path(config["Paths"]["export_format"])
+
+    batch_process_anims(
+        script_path=SCRIPT_PATH,
+        blender_path=BLENDER_PATH,
+        scene_path=SCENE_PATH,
+        body_path=BODY_PATH,
+        anims_dir=ANIMS_DIR,
+        export_dir=EXPORT_DIR,
+        export_type=EXPORT_TYPE,
+        max_workers=max_processes,
+    )
+
+
+if __name__ == "__main__":
+    main()
